e94a6be4407a90e72d7f57e3f19372614737a05d
Vinson Wong <vinson.wong93@gmail.com>
2022-09-28 22:30:58 -0400

    fix

--- fetcher.js
+++ fetcher.js
@@ -3,19 +3,19 @@ const request = require('request');
 const url = process.argv[2];
 const localFile = process.argv[3];
 
-if (process.argv.length < 4) {
+if (process.argv.length !== 4) {
   console.log('Please enter a URL and local file path!');
   return;
 }
 const requestCallback = function(error, response, body) {
   if (!response || response.statusCode !== 200) {
-    console.log('Error:', response && response.statusCode);
+    console.log('error:', response && response.statusCode);
     return;
   }
   console.log(`Downloaded and saved ${body.length} bytes to ${localFile}`);
   fs.writeFile(localFile, body, function(error) {
     if (error) {
-      console.log('Error!', error);
+      console.log('error!', error);
     }
   });
 };
